f52bf24cbeadb71523a13f490c080d2026513732
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2016-12-26 17:18:20 +0700

    new product

--- sites/all/themes/bootstrap/templates/system/page.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page.tpl.php
@@ -126,6 +126,28 @@
 <div class="space-30 font-1 clearfix">
     <div class="main-container <?php print $container_class; ?>">
         <div class="row">
+            <?php if (!empty($page['highlighted'])): ?>
+                <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+            <?php endif; ?>
+
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php if (!empty($tabs)): ?>
+                <?php print render($tabs); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['help'])): ?>
+                <?php print render($page['help']); ?>
+            <?php endif; ?>
+            <?php if (!empty($action_links)): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
+
+            <?php print render($title_prefix); ?>
+            <?php if (!empty($title)): ?>
+                <h1 class="page-header"><?php print $title; ?></h1>
+            <?php endif; ?>
+
             <?php print render($page['content']); ?>
         </div>
     </div>
